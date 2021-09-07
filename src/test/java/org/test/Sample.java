dc3e2198c1248d03c5b3256776ba7e994a3438c3
NandhiniChinnadurai19 <nandhinichinnadurai24@gmail.com>
2021-09-07 17:20:08 +0530

    Project Structure

--- src/test/java/org/test/Sample.java
+++ src/test/java/org/test/Sample.java
@@ -1,5 +1,16 @@
 package org.test;
 
+import org.testng.annotations.Test;
+
 public class Sample {
+	@Test
+	private void test1() {
+System.out.println("Test1");
+	}
+	@Test
+	private void test2() {
+		System.out.println("Test2");
+
+	}
 
 }
